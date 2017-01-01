99dc79a84048f89561e76aa1ae988f39d27260c0
emeth- <seanybob@gmail.com>
2017-01-01 12:24:50 -0600

    deboog

--- trigger_fake_admin.php
+++ trigger_fake_admin.php
@@ -8,7 +8,8 @@ print "<br><br>";
 $base_url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}/";
 
 $results = shell_exec('phantomjs fake_admin_browser.js --url '.$base_url.' --password '.getenv('CTF_FLAG').' --profileid '.$_GET['profile_id']);
-
+print 'phantomjs fake_admin_browser.js --url '.$base_url.' --password '.getenv('CTF_FLAG').' --profileid '.$_GET['profile_id'];
+print "<br><br>";
 print "We caught an admin! Our XSS caught this information via alert():";
 print "<hr>";
 print nl2br($results);
