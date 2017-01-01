9c956075fc3256b918e445ea5246f5f698831580
emeth- <seanybob@gmail.com>
2017-01-01 12:28:44 -0600

    eh

--- trigger_fake_admin.php
+++ trigger_fake_admin.php
@@ -1,16 +1,6 @@
-<?php
+The trap is laid! Waiting for an admin to visit your profile...
+<br><br>
+<script>
+    window.location.href="trigger_fake_admin_2.php?id=<?=$_GET['id']?>";
+</script>
 
-include "mysql.php";
-
-print "The trap is laid! Waiting for an admin to visit your profile...";
-print "<br><br>";
-
-$base_url = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}/";
-
-$results = shell_exec('phantomjs fake_admin_browser.js --url '.$base_url.' --password '.getenv('CTF_FLAG').' --profileid '.$_GET['profile_id']);
-print 'phantomjs fake_admin_browser.js --url '.$base_url.' --password '.getenv('CTF_FLAG').' --profileid '.$_GET['profile_id'];
-print "<br><br>";
-print "We caught an admin! Our XSS caught this information via alert():";
-print "<hr>";
-print nl2br($results);
-print "<hr>";
